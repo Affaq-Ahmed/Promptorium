2a20fa81c7f94b5167a0ff62eed5c8136a9f5b0b
Affaq-Ahmed <aaawan165@gmail.com>
2024-03-23 12:11:35 +0500

    Add user model definition

---
+++ models/user.js
@@ -0,0 +1,25 @@
+import { Schema, model, models } from 'mongoose';
+
+const userSchema = new Schema({
+	email: {
+		type: String,
+		required: [true, 'Please provide an email'],
+		unique: [true, 'Email already exists'],
+		lowercase: true,
+	},
+	username: {
+		type: String,
+		required: [true, 'Please provide a username'],
+		match: [
+			/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+			'Username invalid, it must be 8-20 alphanumeric letters and be unique!',
+		],
+	},
+	image: {
+		type: String,
+	},
+});
+
+const User = models.User || model('User', userSchema);
+
+export default User;
