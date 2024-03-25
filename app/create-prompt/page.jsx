42581542e268752172b88fbcdfe75c323d567cce
Affaq-Ahmed <aaawan165@gmail.com>
2024-03-25 13:42:23 +0500

    Refactor CreatePrompt component to use hooks and add form submission logic

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,7 +1,58 @@
-import React from 'react';
+'use client';
+
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Form from '@components/Form';
 
 const CreatePrompt = () => {
-	return <div>CreatePrompt</div>;
+	const [submitting, setSubmitting] = useState(false);
+	const [post, setPost] = useState({
+		prompt: '',
+		tag: '',
+	});
+
+	const createPrompt = async (e) => {
+		e.preventDefault();
+
+		setSubmitting(true);
+
+		try {
+			const response = await fetch('/api/prompt', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					prompt: post.prompt,
+					tag: post.tag,
+					userId: session?.user.id,
+				}),
+			});
+
+			if (!response.ok) {
+				throw new Error('An error occurred. Please try again.');
+			}
+
+			router.push('/');
+		} catch (error) {
+			console.log(error);
+			alert('An error occurred. Please try again.');
+		} finally {
+      setSubmitting(false);
+    }
+	};
+
+	return (
+		<Form
+			type='Create'
+			post={post}
+			setPost={setPost}
+			submitting={submitting}
+			handleSubmit={createPrompt}
+		/>
+	);
 };
 
 export default CreatePrompt;
