bcca14cb816c820c1a3f61cd35a1631d28fbe7b6
Affaq-Ahmed <aaawan165@gmail.com>
2024-03-27 11:36:55 +0500

    Add route to fetch user posts

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,21 @@
+import { Prompt } from '@models/prompt';
+import { connectToDB } from '@utils/database';
+
+export const GET = async (req, { params }) => {
+	try {
+		await connectToDB();
+
+		console.log(params.id);
+
+		const prompts = await Prompt.find({
+			creator: params.id,
+		}).populate('creator');
+
+		return new Response(JSON.stringify(prompts), { status: 200 });
+	} catch (error) {
+		return new Response(
+			JSON.stringify('Failed to fetch user posts.'),
+			{ status: 500 }
+		);
+	}
+};
