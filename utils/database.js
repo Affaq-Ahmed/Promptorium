60b9c68b226ff150809f1de078c2d690ff747ac5
Affaq-Ahmed <aaawan165@gmail.com>
2024-03-27 13:10:15 +0500

    Remove unused options from mongoose.connect() call

--- utils/database.js
+++ utils/database.js
@@ -12,8 +12,6 @@ export const connectToDB = async () => {
 	try {
 		await mongoose.connect(process.env.MONGODB_URI, {
 			name: 'promptorium',
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
 		});
 
 		isConnected = true;
